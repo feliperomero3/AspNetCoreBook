a702f56b4ad37ec1f321d93b584de3e6096a1865
Felipe Romero <feliperomero3@outlook.com>
2022-05-16 22:03:20 -0500

    Preparing the Application Services and the Request Pipeline.

--- SportsStore/src/Program.cs
+++ SportsStore/src/Program.cs
@@ -1,6 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.MapRazorPages();
 
 app.Run();
