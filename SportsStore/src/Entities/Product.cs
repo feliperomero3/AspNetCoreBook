c27e2d210a1a6cb3286122092f9490a97501b49a
Felipe Romero <feliperomero3@outlook.com>
2022-05-17 23:41:47 -0500

    Starting the Data Model.

---
+++ SportsStore/src/Entities/Product.cs
@@ -0,0 +1,15 @@
+﻿namespace SportsStore.Entities
+{
+    public class Product
+    {
+        public long ProductId { get; }
+
+        public string Name { get; } = String.Empty;
+
+        public string Description { get; } = String.Empty;
+
+        public decimal Price { get; }
+
+        public string Category { get; } = String.Empty;
+    }
+}
