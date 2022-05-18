54855f2cfd99d297a9b330d7af7ff10c6c91f1a5
Felipe Romero <feliperomero3@outlook.com>
2022-05-18 00:15:03 -0500

    Creating the Database Context Class.

---
+++ SportsStore/src/Data/StoreDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SportsStore.Entities;
+
+namespace SportsStore.Data
+{
+    public class StoreDbContext : DbContext
+    {
+        public StoreDbContext(DbContextOptions<StoreDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products => Set<Product>();
+    }
+}
