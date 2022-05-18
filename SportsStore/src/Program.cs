6c66768ebc312b9a3865b997f243dc205f1252c7
Felipe Romero <feliperomero3@outlook.com>
2022-05-18 00:24:08 -0500

    Configuring Entity Framework Core.

--- SportsStore/src/Program.cs
+++ SportsStore/src/Program.cs
@@ -1,6 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using SportsStore.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorPages();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<StoreDbContext>(options => options.UseSqlServer(connectionString));
 
 var app = builder.Build();
 
