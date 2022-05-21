216b2d957f3034d8dc76da4ecb2370fab7e9f6e4
Felipe Romero <feliperomero3@outlook.com>
2022-05-20 20:31:59 -0500

    Displaying a List of Products.

--- SportsStore/src/Pages/Index.cshtml.cs
+++ SportsStore/src/Pages/Index.cshtml.cs
@@ -1,11 +1,24 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SportsStore.Data;
+using SportsStore.Entities;
 
 namespace SportsStore.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly StoreDbContext _dbContext;
+
+        public IndexModel(StoreDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IEnumerable<Product> Products { get; private set; } = Enumerable.Empty<Product>();
+
         public void OnGet()
         {
+            Products = _dbContext.Products.AsNoTracking().ToArray();
         }
     }
 }
