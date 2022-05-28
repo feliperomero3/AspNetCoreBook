6946baa3cb0d6097a4bd51756f41edb485732a0a
Felipe Romero <feliperomero3@outlook.com>
2022-05-28 00:33:26 -0500

    Creating the Navigation View Component.

---
+++ SportsStore/src/ViewComponents/NavigationMenuViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SportsStore.ViewComponents
+{
+    public class NavigationMenuViewComponent : ViewComponent
+    {
+        public string Invoke()
+        {
+            return "Hello from the Navigation View Component.";
+        }
+    }
+}
