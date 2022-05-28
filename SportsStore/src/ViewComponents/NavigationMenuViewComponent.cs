dec9f3ad1b63f6827550889167744476b8d9f79f
Felipe Romero <feliperomero3@outlook.com>
2022-05-28 18:17:40 -0500

    Highlighting the Current Category.

--- SportsStore/src/ViewComponents/NavigationMenuViewComponent.cs
+++ SportsStore/src/ViewComponents/NavigationMenuViewComponent.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Primitives;
 using SportsStore.Data;
 
 namespace SportsStore.ViewComponents;
@@ -7,14 +8,29 @@ namespace SportsStore.ViewComponents;
 public class NavigationMenuViewComponent : ViewComponent
 {
     private readonly StoreDbContext _dbContext;
+    private readonly ILogger<NavigationMenuViewComponent> _logger;
 
-    public NavigationMenuViewComponent(StoreDbContext context)
+    public NavigationMenuViewComponent(StoreDbContext context, ILogger<NavigationMenuViewComponent> logger)
     {
         _dbContext = context;
+        _logger = logger;
     }
 
     public async Task<IViewComponentResult> InvokeAsync()
     {
+        var category = Request.Query["category"];
+
+        if (category == StringValues.Empty)
+        {
+            _logger.LogInformation("No Category found in query string.");
+        }
+        else
+        {
+            _logger.LogInformation("Category '{category}' found in query string.", category);
+        }
+
+        ViewBag.SelectedCategory = category;
+
         var categories = await _dbContext.Products
             .Select(p => p.Category)
             .Distinct()
