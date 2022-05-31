7c95543df8e7b29eb73fa26e2ae0608f1584c7ed
Felipe Romero <feliperomero3@outlook.com>
2022-05-31 00:37:24 -0500

    Add empty string as default value for product model.

--- SportsStore/src/Models/ProductModel.cs
+++ SportsStore/src/Models/ProductModel.cs
@@ -7,14 +7,14 @@ public class ProductModel
 {
     public long ProductId { get; set; }
 
-    public string? Name { get; set; }
+    public string Name { get; set; } = string.Empty;
 
-    public string? Description { get; set; }
+    public string Description { get; set; } = string.Empty;
 
     [DataType(DataType.Currency)]
     public decimal Price { get; set; }
 
-    public string? Category { get; set; }
+    public string Category { get; set; } = string.Empty;
 
     public static ProductModel MapFromProduct(Product product)
     {
