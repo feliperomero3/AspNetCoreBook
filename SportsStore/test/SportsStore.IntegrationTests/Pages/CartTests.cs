c982716c23965af42537fa5cdc25cd0d2960e018
Felipe Romero <feliperomero3@outlook.com>
2022-06-02 23:16:01 -0500

    Extract cart integration tests class.

---
+++ SportsStore/test/SportsStore.IntegrationTests/Pages/CartTests.cs
@@ -0,0 +1,65 @@
+﻿using AngleSharp.Html.Dom;
+using SportsStore.IntegrationTests.Helpers;
+
+namespace SportsStore.IntegrationTests.Pages;
+
+public class CartTests : IClassFixture<CustomWebApplicationFactory<Program>>
+{
+    private readonly CustomWebApplicationFactory<Program> _factory;
+
+    public CartTests(CustomWebApplicationFactory<Program> factory)
+    {
+        _factory = factory;
+    }
+
+    [Fact]
+    public async Task Add_Product_to_Cart()
+    {
+        var client = _factory.CreateClient();
+
+        var homePageResponse = await client.GetAsync(string.Empty);
+
+        var homePage = await HtmlDocumentHelper.GetDocumentAsync(homePageResponse);
+
+        var form = (IHtmlFormElement)homePage.QuerySelector("form")!;
+        var submit = (IHtmlButtonElement)homePage.QuerySelector("button[type='submit']")!;
+
+        var cartPageResponse = await client.SendAsync(form, submit);
+
+        var cartPage = await HtmlDocumentHelper.GetDocumentAsync(cartPageResponse);
+
+        var lines = cartPage.QuerySelector("#cart #lines");
+
+        Assert.NotNull(lines);
+        Assert.Equal(1, lines!.Children.Length);
+    }
+
+    [Fact]
+    public async Task Remove_Product_from_Cart()
+    {
+        var client = _factory.CreateClient();
+
+        var homePageResponse = await client.GetAsync(string.Empty);
+
+        var homePage = await HtmlDocumentHelper.GetDocumentAsync(homePageResponse);
+
+        var form = (IHtmlFormElement)homePage.QuerySelector("form")!;
+        var submit = (IHtmlButtonElement)homePage.QuerySelector("button[type='submit']")!;
+
+        var cartPageResponse = await client.SendAsync(form, submit);
+
+        var cartPage = await HtmlDocumentHelper.GetDocumentAsync(cartPageResponse);
+
+        var formCartPage = (IHtmlFormElement)cartPage.QuerySelector("form")!;
+        var submitCartPage = (IHtmlButtonElement)cartPage.QuerySelector("button[type='submit']")!;
+
+        var cartPage2Response = await client.SendAsync(formCartPage, submitCartPage);
+
+        var cartPage2 = await HtmlDocumentHelper.GetDocumentAsync(cartPage2Response);
+
+        var lines = cartPage2.QuerySelector("#cart #lines");
+
+        Assert.NotNull(lines);
+        Assert.Equal(0, lines!.Children.Length);
+    }
+}
--- SportsStore/test/SportsStore.IntegrationTests/Pages/IndexTests.cs
+++ SportsStore/test/SportsStore.IntegrationTests/Pages/IndexTests.cs
@@ -1,5 +1,4 @@
 ﻿using System.Net;
-using AngleSharp.Html.Dom;
 using SportsStore.IntegrationTests.Helpers;
 
 namespace SportsStore.IntegrationTests.Pages;
@@ -37,56 +36,5 @@ public class IndexTests : IClassFixture<CustomWebApplicationFactory<Program>>
         var products = content.QuerySelectorAll("#products .card");
 
         Assert.Equal(4, products?.Length);
-    }
-
-    [Fact]
-    public async Task Add_Product_to_Cart()
-    {
-        var client = _factory.CreateClient();
-
-        var homePageResponse = await client.GetAsync(string.Empty);
-
-        var homePage = await HtmlDocumentHelper.GetDocumentAsync(homePageResponse);
-
-        var form = (IHtmlFormElement)homePage.QuerySelector("form")!;
-        var submit = (IHtmlButtonElement)homePage.QuerySelector("button[type='submit']")!;
-
-        var cartPageResponse = await client.SendAsync(form, submit, new Dictionary<string, string> { });
-
-        var cartPage = await HtmlDocumentHelper.GetDocumentAsync(cartPageResponse);
-
-        var lines = cartPage.QuerySelector("#cart #lines");
-
-        Assert.NotNull(lines);
-        Assert.Equal(1, lines!.Children.Length);
-    }
-
-    [Fact]
-    public async Task Remove_Product_from_Cart()
-    {
-        var client = _factory.CreateClient();
-
-        var homePageResponse = await client.GetAsync(string.Empty);
-
-        var homePage = await HtmlDocumentHelper.GetDocumentAsync(homePageResponse);
-
-        var form = (IHtmlFormElement)homePage.QuerySelector("form")!;
-        var submit = (IHtmlButtonElement)homePage.QuerySelector("button[type='submit']")!;
-
-        var cartPageResponse = await client.SendAsync(form, submit);
-
-        var cartPage = await HtmlDocumentHelper.GetDocumentAsync(cartPageResponse);
-
-        var formCartPage = (IHtmlFormElement)cartPage.QuerySelector("form")!;
-        var submitCartPage = (IHtmlButtonElement)cartPage.QuerySelector("button[type='submit']")!;
-
-        var cartPage2Response = await client.SendAsync(formCartPage, submitCartPage);
-
-        var cartPage2 = await HtmlDocumentHelper.GetDocumentAsync(cartPage2Response);
-
-        var lines = cartPage2.QuerySelector("#cart #lines");
-
-        Assert.NotNull(lines);
-        Assert.Equal(0, lines!.Children.Length);
     }
 }
