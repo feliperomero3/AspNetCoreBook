6185ee41b97d2b4bb1837f86f55d036fa4dac3c4
Felipe Romero <feliperomero3@outlook.com>
2022-06-04 20:20:18 -0500

    Creating the Order Class.

---
+++ SportsStore/src/Entities/Order.cs
@@ -0,0 +1,26 @@
+﻿namespace SportsStore.Entities;
+
+public class Order
+{
+    public long OrderID { get; set; }
+
+    public ICollection<CartLine> Lines { get; set; } = new List<CartLine>();
+
+    public string Name { get; set; } = string.Empty;
+
+    public string Line1 { get; set; } = string.Empty;
+
+    public string Line2 { get; set; } = string.Empty;
+
+    public string Line3 { get; set; } = string.Empty;
+
+    public string City { get; set; } = string.Empty;
+
+    public string State { get; set; } = string.Empty;
+
+    public string Zip { get; set; } = string.Empty;
+
+    public string Country { get; set; } = string.Empty;
+
+    public bool GiftWrap { get; set; }
+}
---
+++ SportsStore/src/Models/OrderModel.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using SportsStore.Entities;
+
+namespace SportsStore.Models;
+
+public class OrderModel
+{
+    [BindNever]
+    public long OrderID { get; set; }
+
+    [BindNever]
+    public ICollection<CartLine> Lines { get; set; } = new List<CartLine>();
+
+    [Required(ErrorMessage = "Please enter a name")]
+    public string Name { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Please enter the first address line")]
+    public string Line1 { get; set; } = string.Empty;
+
+    public string Line2 { get; set; } = string.Empty;
+
+    public string Line3 { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Please enter a city name")]
+    public string City { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Please enter a state name")]
+    public string State { get; set; } = string.Empty;
+
+    public string Zip { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Please enter a country name")]
+    public string Country { get; set; } = string.Empty;
+
+    public bool GiftWrap { get; set; }
+}
